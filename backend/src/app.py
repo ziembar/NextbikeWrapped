d7e486114a0990b77d46155c4104d18466d6a455
bartosz.ziemba <bartosz.ziemba@radrat.pl>
2024-03-22 14:12:42 +0100

    chore: initial commit

---
+++ backend/src/app.py
@@ -0,0 +1,25 @@
+import os
+from settings import settings
+from flask import Flask
+
+def create_app():
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get('GOOGLE_API_KEY'),
+    )
+   
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
+
+    return app
