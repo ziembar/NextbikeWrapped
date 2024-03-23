1b2456344bd8517913edc3307c0aed05d122b2bf
bartosz.ziemba <bartosz.ziemba@radrat.pl>
2024-03-23 20:10:18 +0100

    feat: obtain cookie

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,8 @@
 import os
 from settings import settings
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+import json
 
 def create_app():
     # create and configure the app
@@ -18,8 +20,30 @@ def create_app():
         pass
 
     # a simple page that says hello
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
+    @app.route('/login', methods=['POST', 'GET'])
+    def login():
+        # phone = request.json['phone']
+        # pin = request.json['pin']
+
+        url = "https://account.nextbike.pl/api/login"
+        headers = {
+            "Accept": "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+            "Origin": "https://account.nextbike.pl",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "cors",
+            "Referer": "https://account.nextbike.pl/pl-PL/vw",
+            "Accept-Language": "en-US,en;q=0.9",
+        }
+        data = {
+            "mobile": "48609990600",
+            "pin": "956324",
+            "city": "vw",
+        }
+
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        response.raise_for_status()
+
+        return response.headers['set-cookie']
 
     return app
