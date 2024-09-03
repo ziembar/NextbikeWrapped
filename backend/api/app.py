7ec9bb3d65d6a926f94935ac4c85c6c9e9eb4e58
bartosz.ziemba <bartoszzziemba@gmail.com>
2024-09-03 22:52:27 +0200

    gaa

--- backend/api/app.py
+++ backend/api/app.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    cors = CORS(app, resources={r"/api/*": {"origins": [config('FRONTEND_URL')]}})
+    # cors = CORS(app, resources={r"/api/*": {"origins": [config('FRONTEND_URL')]}})
    
     try:
         os.makedirs(app.instance_path)
